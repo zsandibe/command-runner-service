90e5e130567b94a801237b9f65b4a13fc50320a4
zsandibe <Sandibekov1998@gmail.com>
2024-07-02 20:56:07 +0500

    finally without readme

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/zsandibe/command-runner-service/internal/app"
+import (
+	"github.com/zsandibe/command-runner-service/internal/app"
+	logger "github.com/zsandibe/command-runner-service/pkg"
+)
 
 func main() {
-	app.Start()
+	if err := app.Start(); err != nil {
+		logger.Error(err)
+		return
+	}
 }
